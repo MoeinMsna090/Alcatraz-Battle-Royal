904a829960a5669bd86228610eb2a5bdab2b84da
MoeinMsna090 <moeinmsna45@gmail.com>
2025-07-15 11:49:12 +0330

    Create AdvancePlayerController.cs

---
+++ AdvancePlayerController.cs
@@ -0,0 +1,77 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour
+{
+    [Header("Movement")]
+    public float walkSpeed = 5f;
+    public float sprintSpeed = 8f;
+    public float jumpForce = 5f;
+    public float gravity = -9.81f;
+    public Transform groundCheck;
+    public LayerMask groundMask;
+
+    [Header("Look")]
+    public float mouseSensitivity = 100f;
+    public Transform playerCamera;
+    public float maxLookAngle = 85f;
+
+    private CharacterController controller;
+    private Vector3 velocity;
+    private bool isGrounded;
+    private float xRotation = 0f;
+    private float currentSpeed;
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
+        currentSpeed = walkSpeed;
+    }
+
+    private void Update()
+    {
+        HandleMovement();
+        HandleLook();
+    }
+
+    private void HandleMovement()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, 0.2f, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+            velocity.y = -2f;
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * currentSpeed * Time.deltaTime);
+
+        // Sprint
+        if (Input.GetKey(KeyCode.LeftShift))
+            currentSpeed = sprintSpeed;
+        else
+            currentSpeed = walkSpeed;
+
+        // Jump
+        if (Input.GetButtonDown("Jump") && isGrounded)
+            velocity.y = Mathf.Sqrt(jumpForce * -2f * gravity);
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    private void HandleLook()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -maxLookAngle, maxLookAngle);
+
+        playerCamera.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        transform.Rotate(Vector3.up * mouseX);
+    }
+}
